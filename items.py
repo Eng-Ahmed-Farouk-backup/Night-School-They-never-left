388be8d938b0f62d970180ff914ea60ff94ae102
Ahmed Farouk <160106737+Eng-Ahmed-Farouk@users.noreply.github.com>
2026-04-08 19:24:43 +0200

    strength potions and boots
    added boots base class that calculates efficiency of every boot according to level
    also added strength potions that increase the player attack damage

--- items.py
+++ items.py
@@ -26,3 +26,47 @@ class HealthPotion2(HealthPotion):
 class HealthPotion3(HealthPotion):
     def __init__(self):
         super().__init__(3)
+
+class StengthPotion(Item):
+    def __init__(self, level):
+        self.level = level
+        super().__init__(f"Strength Potion {self.level}", f"Increase attack by {2*(2**(self.level-1))}", 15*(2**(self.level-1)), 7*(2**(self.level-1)))
+    def use(self, player):
+        player.base_attack += 2*(2**(self.level-1))
+        player.inventory.remove(self)
+
+class StrengthPotion1(Item):
+    def __init__(self):
+        super().__init__(1)
+
+class StrengthPotion2(Item):
+    def __init__(self):
+        super().__init__(2)
+
+class StrengthPotion3(Item):
+    def __init__(self):
+        super().__init__(3)
+
+
+class Boots(Item):
+    def __init__(self, level):
+        self.level = level
+        super().__init__(f"Boots {self.level}", f"Increase agility by {1*(2**(self.level-1))}", 20*(2**(self.level-1)), 10*(2**(self.level-1)))
+    def use(self, player):
+        player.base_agility += 0.25 * self.level
+
+class Boots1(Boots):
+    def __init__(self):
+        super().__init__(1)
+
+class Boots2(Boots):
+    def __init__(self):
+        super().__init__(2)
+
+class Botts3(Boots):
+    def __init__(self):
+        super().__init__(3)
+
+
+    
+    
